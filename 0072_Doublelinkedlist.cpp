53a48fcd1d410698e3983fa3c358556751cc4220
Yurcell <chestayurcel@gmail.com>
2024-05-04 01:18:57 +0700

    membuat else start = newnode

--- 0072_Doublelinkedlist.cpp
+++ 0072_Doublelinkedlist.cpp
@@ -58,5 +58,10 @@ void addNode()
         {
             previous->next = newNode; // step 7: make the next field of the previous node point to the new node
         }
+        else
+        {
+            // if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
     }
 }
